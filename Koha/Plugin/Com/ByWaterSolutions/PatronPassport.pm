5a0a56684c641df9291727aca3a7d1d82beea7f4
Kyle M Hall <kyle@bywatersolutions.com>
2022-02-04 12:54:21 -0500

    Add functionality

--- Koha/Plugin/Com/ByWaterSolutions/PatronPassport.pm
+++ Koha/Plugin/Com/ByWaterSolutions/PatronPassport.pm
@@ -9,10 +9,18 @@ use base qw(Koha::Plugins::Base);
 use Cwd qw(abs_path);
 use Encode qw(decode);
 use File::Slurp qw(read_file);
-use Module::Metadata;
+use YAML::XS;
+
+use HTTP::Request::Common;
+use LWP::UserAgent;
+use Parallel::Loops;
+use Try::Tiny;
 
 use C4::Auth;
 use C4::Context;
+use Koha::Patrons;
+use Koha::Patron::Attribute::Types;
+use Koha::Patron::Attributes;
 
 our $VERSION         = "{VERSION}";
 our $MINIMUM_VERSION = "{MINIMUM_VERSION}";
@@ -20,12 +28,13 @@ our $MINIMUM_VERSION = "{MINIMUM_VERSION}";
 our $metadata = {
     name            => 'Patron Passport',
     author          => 'Kyle M Hall, ByWater Solutions',
-    date_authored   => '2021-01-25',
+    date_authored   => '2021-02-03',
     date_updated    => "1900-01-01",
     minimum_version => $MINIMUM_VERSION,
     maximum_version => undef,
     version         => $VERSION,
-    description     => 'Allows separate instances of Koha to automatically import patrons from other Koha servers',
+    description =>
+'Enable separate instances of Koha to automatically import patrons from other Koha servers',
 };
 
 sub new {
@@ -39,12 +48,157 @@ sub new {
     return $self;
 }
 
+sub patron_barcode_transform {
+    my ( $self, $cardnumber_ref ) = @_;
 
-sub install() {
+    # we only want to look up externally if we are at the checkout screen
+    my ($package, $filename, $line) = caller 3;
+    return unless $filename =~ /circulation.pl$/;
+
+    my $cardnumber = $$cardnumber_ref;
+
+    my $patron = Koha::Patrons->search(
+        {
+            -or => [
+                cardnumber => $cardnumber,
+                userid     => $cardnumber,
+            ]
+        }
+    )->single;
+
+    return if $patron;    # Patron exists
+
+    my $conf    = C4::Context->config('patron_passport');
+    my $servers = $conf->{servers}->{server};
+
+    my $pl = Parallel::Loops->new(99);
+    my @returnValues;
+    $pl->share( \@returnValues );
+
+    $pl->foreach(
+        $servers,
+        sub {
+            my $ua = LWP::UserAgent->new();
+
+            my $address  = $_->{address};
+            my $username = $_->{username};
+            my $password = $_->{password};
+
+            my $request;
+            my $url = "$address/api/v1/contrib/patron_passport/patron/check/$cardnumber";
+            try {
+                $request = GET $url;
+            }
+            catch {
+                warn "caught error: $_";
+            };
+
+            $request->authorization_basic( $username, $password );
+
+            my $response = $ua->request($request);
+            push(
+                @returnValues,
+                {
+                    server   => $_,
+                    response => $response,
+                }
+            );
+        }
+    );
+
+    my $patron_data;
+    my $server;
+    foreach my $d (@returnValues) {
+        my $r = $d->{response};
+        if ( $r->is_success && $r->code eq '200' ) {
+            $patron_data = decode_json( $d->{response}->decoded_content );
+            $server = $d->{server};
+            last;
+        }
+    }
+
+    my $settings = { map { $_->{name} => $_->{value} } @{ $conf->{setting} } };
+
+    delete $patron_data->{borrowernumber};
+
+    $patron_data->{branchcode} = $settings->{default_branchcode}
+      if $settings->{default_branchcode};
+    $patron_data->{categorycode} = $settings->{default_categorycode}
+      if $settings->{default_categorycode};
+
+    $patron_data->{branchcode} =
+        C4::Context->userenv
+      ? C4::Context->userenv->{'branch'}
+      : $patron_data->{branchcode}
+      if $settings->{use_logged_in_branchcode};
+
+    try {
+        my $patron = Koha::Patron->new($patron_data)->store();
+        $patron->add_extended_attribute({ code => 'PASSPORTED', attribute => $server->{name} });
+    }
+    catch {
+        warn "caught error: $_";
+    };
+}
+
+sub cronjob_nightly {
+    my ( $self ) = @_;
+
+    my $conf    = C4::Context->config('patron_passport');
+    my $servers = { map { $_->{name} => $_ } @{ $conf->{servers}->{server}  } };
+    my $settings = { map { $_->{name} => $_->{value} } @{ $conf->{setting} } };
+
+    my $attrs = Koha::Patron::Attributes->search({ code => 'PASSPORTED' }); 
+    while ( my $a = $attrs->next ) {
+        next unless $a->attribute;
+
+        my $server = $servers->{$a->attribute};
+        next unless $server;
+
+        my $patron = Koha::Patrons->find( $a->borrowernumber );
+        next unless $patron;
+
+        my $ua = LWP::UserAgent->new();
+
+        my $address  = $server->{address};
+        my $username = $server->{username};
+        my $password = $server->{password};
+
+        my $cardnumber = $patron->cardnumber;
+
+        my $request;
+        my $url = "$address/api/v1/contrib/patron_passport/patron/check/$cardnumber";
+        try {
+            $request = GET $url;
+        }
+        catch {
+            warn "caught error: $_";
+        };
+
+        $request->authorization_basic( $username, $password );
+
+        my $response = $ua->request($request);
+        next unless $response->is_success && $response->code eq '200';
+
+        my $patron_data = decode_json( $response->decoded_content );
+        delete $patron_data->{borrowernumber};
+        delete $patron_data->{categorycode};
+        $patron->update($patron_data);
+    }
+}
+
+sub install {
     my ( $self, $args ) = @_;
 
-    my $dbh = C4::Context->dbh;
-    $dbh->do(q{INSERT IGNORE INTO `borrower_attribute_types` VALUES ('PASSPORTED','Imported from other ILS',0,0,0,0,0,'YES_NO',0,NULL,'',0,0)});
+    my $attribute_type = Koha::Patron::Attribute::Type->find('PASSPORTED');
+    Koha::Patron::Attribute::Type->new(
+        {
+            code        => 'PASSPORTED',
+            description => 'ILS patron was imported from',
+        }
+    )->store()
+      unless $attribute_type;
+
     return 1;
 }
 
@@ -53,7 +207,7 @@ sub upgrade {
     return 1;
 }
 
-sub uninstall() {
+sub uninstall {
     my ( $self, $args ) = @_;
     return 1;
 }
@@ -61,8 +215,8 @@ sub uninstall() {
 sub api_routes {
     my ( $self, $args ) = @_;
 
-    my $spec_str = $self->mbf_read('openapi.json');
-    my $spec     = decode_json($spec_str);
+    my $spec_str = $self->mbf_read('openapi.yaml');
+    my $spec     = Load($spec_str);
 
     return $spec;
 }
--- README.md
+++ README.md
@@ -6,5 +6,26 @@ to find the server that has that patron, and will close the patron from it.
 
 ## Installation
 
-This plugin requires data to be added to koha-conf.xml in the following format:
+* Enable RESTBasicAuth on all Koha instances
 
+This plugin requires data to be added to koha-conf.xml in the following format:
+```xml
+ <patron_passport>
+    <setting name="default_category_code" value="PT"/>
+    <setting name="default_branchcode" value="MPL"/>
+    <setting name="use_logged_in_branchcode" value="1"/> <!-- has precedence over default branchcode -->
+
+    <servers>
+        <server name="ServerB" address="libB.libraries.org" username="koha" password="koha" />
+        <server name="ServerC" address="libC.libraries.org" username="koha" password="koha" />
+    </servers>
+ </patron_passport>
+```
+this must be inside the `config` block of the koha conf.
+
+## Setup
+
+It is important that all the possible branchcodes and category codes be set up in all Koha instances
+unless you are using the settings to replace the external codes with your own internal ones.
+Any attept to import a patron with a branchcode or category code that is not valid in your Koha
+instance will fail.
